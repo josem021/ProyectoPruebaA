d296537d3287f21f4d70e335105e46c7b0bd05a4
JoseM021 <122744449+JoseM021@users.noreply.github.com>
2024-01-31 22:52:55 -0500

    Actualización de la pagina a tematica a usar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  titleHeader = 'Titulo Header';
+  titleHeader = 'Cinereview';
 }
