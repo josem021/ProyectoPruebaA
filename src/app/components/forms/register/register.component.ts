c26313d44ae1cb5ee9e1b7e7acd5cc0c70353efb
josem021 <joossec21j@gmail.com>
2024-02-16 17:04:02 -0500

    Validators 50%

--- src/app/components/forms/register/register.component.ts
+++ src/app/components/forms/register/register.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registrationForm = new FormGroup({
+    correoElectronico: new FormControl ('',[Validators.required, Validators.email]),
+    confirmarCorreo: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required, Validators.minLength(6)]),
+    username: new FormControl('',Validators.required)
+  });
 
+  clickRegister(dateRegister: any){
+    console.log(dateRegister);
+  }
 }
